6539980b49fe14cdb558c410f5198720fd51ea29
Isaac Barrett <ikebart9999@gmail.com>
2013-09-25 17:32:09 -0400

    Added four methods to MOAISpriteCache.

--- src/moaicore/MOAISpriteCache.cpp
+++ src/moaicore/MOAISpriteCache.cpp
@@ -9,6 +9,58 @@
 #include "pch.h"
 #include <moaicore/MOAISpriteCache.h>
 
+//----------------------------------------------------------------//
+/** @name	loadSpritesheetWithName
+	@text
+	
+	@in		string name
+	@out	nil
+ */
+int MOAISpriteCache::_loadSpritesheetWithName(lua_State *L){
+	MOAILuaState state( L );
+	cc8 *name = state.GetValue < cc8* > (1, "");
+	
+	return 0;
+}
+
+//----------------------------------------------------------------//
+/** @name	loadTextureWithFilename
+	@text
+ 
+	@in		string filename
+	@out	nil
+ */
+int MOAISpriteCache::_loadTextureWithFilename(lua_State *L){
+	MOAILuaState state( L );
+	return 0;
+}
+
+//----------------------------------------------------------------//
+/** @name	uncacheSpritesheetWithName
+	@text 
+ 
+	@in		string name
+	@out	nil
+ */
+int MOAISpriteCache::_uncacheSpritesheetWithName(lua_State *L){
+	MOAILuaState state( L );
+	return 0;
+}
+
+//----------------------------------------------------------------//
+/** @name	uncacheTextureWithFilename
+	@text	Remove the texture indexed with the filename from the cache.
+ 
+	@in		string filename
+	@out	nil
+ */
+int MOAISpriteCache::_uncacheTextureWithFilename(lua_State *L){
+	MOAILuaState state( L );
+	return 0;
+}
+
+//----------------------------------------------------------------//
+
 
 MOAISpriteCache::MOAISpriteCache(){
 	RTTI_BEGIN
@@ -24,6 +76,10 @@ MOAISpriteCache::~MOAISpriteCache(){
 
 void MOAISpriteCache::RegisterLuaClass(MOAILuaState &state){
 	luaL_Reg regTable [] = {
+		{ "loadSpritesheeetWithName", _loadSpritesheetWithName },
+		{ "loadTextureWithFilename", _loadTextureWithFilename },
+		{ "uncacheSpritesheetWithName", _uncacheSpritesheetWithName },
+		{ "uncacheTextureWithFilename", _uncacheTextureWithFilename },
 		{ NULL, NULL }
 	};
 	
--- src/moaicore/MOAISpriteCache.h
+++ src/moaicore/MOAISpriteCache.h
@@ -11,12 +11,23 @@
 
 #include <moaicore/MOAILua.h>
 
+class MOAIDeck;
+
 // a class that holds cached sprites
 class MOAISpriteCache : public MOAIGlobalClass <MOAISpriteCache, MOAILuaObject> {
 	
 private:
 	
+	static int _loadSpritesheetWithName ( lua_State *L );
+	static int _loadTextureWithFilename ( lua_State *L );
+	
+	static int _uncacheSpritesheetWithName ( lua_State *L );
+	static int _uncacheTextureWithFilename ( lua_State *L );
+	
+	
 //----------------------------------------------------------------//
+	
+	STLMap <STLString, MOAIDeck > mCachedSpriteDefinitons;
 	
 public:
 	
